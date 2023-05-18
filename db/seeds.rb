f877d4af0e311a4ebc3ca9f4af7d01e45bec68e2
Chris Simmons <chris@turing.edu>
2023-05-18 17:29:38 -0400

    Adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+AmusementPark.destroy_all
+Ride.destroy_all
+
+@six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+@universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
+
+@hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+@scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+@ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+
+@jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
