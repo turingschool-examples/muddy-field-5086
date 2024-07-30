5c002afbb3b73a8d39dd577041538c30d8c80618
Karl Fallenius <smilodon.p@gmail.com>
2024-07-30 14:05:54 -0600

    adds most of ic setup

---
+++ spec/features/mechanic/show_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "Mechanic Show Page:", type: :feature do
+  before(:each) do
+    
+  end
+end
---
+++ spec/models/mechanics_ride_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe MechanicsRide, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
