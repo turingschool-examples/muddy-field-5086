ae8a5edf7766859bb579b5b0089a985754fbff8b
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-30 15:12:31 -0600

    Refcator test data

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -3,21 +3,25 @@ require "rails_helper"
 RSpec.describe "the mechanic show" do
 
   before :each do
-    @mechanic1 = Mechanic.create!(name: "Johny", years_experience: 10)
-    @mechanic2 = Mechanic.create!(name: "Billy", years_experience: 5)
-
-    @ride1 = Ride.create!(name: "Crazy Monkey", thrill_rating: 10, open: true)
-    @ride2 = Ride.create!(name: "Rocket", thrill_rating: 9, open: true)
-    @ride3 = Ride.create!(name: "Something Good", thrill_rating: 13, open: false)
-    @ride4 = Ride.create!(name: "Death from Above", thrill_rating: 15, open: true)
-    @ride5 = Ride.create!(name: "The Hurler", thrill_rating: 7, open: false)
-
-    @mechanic_ride1 = MechanicRide.create!(mechanic: @mechanic1, ride: @ride1)
-    @mechanic_ride2 = MechanicRide.create!(mechanic: @mechanic1, ride: @ride2)
-    @mechanic_ride3 = MechanicRide.create!(mechanic: @mechanic1, ride: @ride3)
-    @mechanic_ride4 = MechanicRide.create!(mechanic: @mechanic2, ride: @ride4)
-    @mechanic_ride5 = MechanicRide.create!(mechanic: @mechanic2, ride: @ride5)
-    @mechanic_ride6 = MechanicRide.create!(mechanic: @mechanic1, ride: @ride4)
+    six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+    universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
+    
+    ride1 = six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+    ride2 = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+    ride3 = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+    
+    ride4 = universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+    ride5 = universal.rides.create!(name: 'Jurrasic Park', thrill_rating: 8, open: true)
+    
+    mechanic1 = Mechanic.create!(name: "Johny", years_experience: 10)
+    mechanic2 = Mechanic.create!(name: "Billy", years_experience: 5)
+    
+    mechanic_ride1 = MechanicRide.create!(mechanic: mechanic1, ride: ride1)
+    mechanic_ride2 = MechanicRide.create!(mechanic: mechanic1, ride: ride2)
+    mechanic_ride3 = MechanicRide.create!(mechanic: mechanic1, ride: ride3)
+    mechanic_ride4 = MechanicRide.create!(mechanic: mechanic2, ride: ride4)
+    mechanic_ride5 = MechanicRide.create!(mechanic: mechanic2, ride: ride5)
+    mechanic_ride6 = MechanicRide.create!(mechanic: mechanic1, ride: ride4)
   end
 
   describe "As a visitor do" do
